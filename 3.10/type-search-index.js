68be6edceddf3bd8ac43e31b7cfc4230dbc6c481
Derk Norton <derk.norton@gmail.com>
2021-11-20 10:39:15 -0700

    Added craterdog/java-core-interfaces version 3.10 of the javadocs.

---
+++ 3.10/type-search-index.js
@@ -0,0 +1,1 @@
+typeSearchIndex = [{"l":"All Classes and Interfaces","u":"allclasses-index.html"},{"p":"craterdog.core","l":"Atomic"},{"p":"craterdog.core","l":"Composite"},{"p":"craterdog.core","l":"Iterator"},{"p":"craterdog.core","l":"Manipulator"},{"p":"craterdog.core","l":"Sequential"}];updateSearchResults();
